2b4567fa0788af552555384cc016e6e180042a07
Lucas <lww515@yahoo.com>
2019-09-07 12:29:02 -0400

    Add file uploading functionality

--- routes/card.js
+++ routes/card.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const ipfsAPI = require('ipfs-api');
+
+// Connecting to the ipfs network via infura gateway
+const ipfs = ipfsAPI('ipfs.infura.io', '5001', {protocol: 'https'})
+
+const upload = require('../uploadMiddleware');
+
 const Web3 = require('web3')
 const provider = new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/02754071220c49e1b2bfeb1eb905af88')
 const web3 = new Web3(provider)
+
+const IPFS_URL = "https://ipfs.infura.io/ipfs/";
 
 let video;
 
@@ -15,7 +24,7 @@ router.get('/:key', function (req, res, next) {
 
   if (recovered == account.address) {
     // Use account.address to get ipfs hash
-    res.render('upload', { private_key :  private_key});
+    res.render('upload', { private_key : private_key });
   } else {
     res.render('message', { message: 'Invalid Signature' });
   }
@@ -23,10 +32,19 @@ router.get('/:key', function (req, res, next) {
 
 
 /* GET home page. */
-router.post('/:key', function (req, res, next) {
-  file = req.body.file;
-  console.log(file);
-  res.render('view', { url :  "https://ipfs.infura.io/ipfs/QmRHDjZC6ZgkDwdKP2b2N984ckhhs2yKehLSz7NmeZv7Rn"});
+router.post('/:key', upload.single('image'), function (req, res, next) {
+  console.log('file:', req.file);
+  const buffer = req.file.buffer;
+  ipfs.files.add(buffer, function (err, file) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(file)
+      var hash = file[0].hash;
+      var fileUrl = IPFS_URL + hash
+      res.render('view', { url : fileUrl });
+    }
+  });
 });
 
 module.exports = router;
