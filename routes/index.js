6685788f5a7176fe8e98c828aa340be701cea71e
Armando <flores@mtu.edu>
2019-09-07 21:35:28 -0400

    UI Styling updates

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 let video;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('message', { message: 'Welcome! Please scan a lobster card to get started!' });
+  res.render('message', { message: 'Please find and scan a Lobster Card to get started.' });
 });
 
 module.exports = router;
